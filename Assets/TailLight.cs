a82c97d00f9c9bba6e47a21c862fb34f1887cfa9
Shreyansh <shreyanshgupta118@gmail.com>
2026-04-24 11:30:53 +0530

    Add tail light to DodoTruck and its logic

---
+++ Assets/TailLight.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal; // Required for Light2D
+
+public class TailLight : MonoBehaviour
+{
+    private Light2D tailLight;
+
+    private void Awake()
+    {
+        tailLight = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        if (tailLight == null) return;
+
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.sKey.isPressed)
+        {
+            tailLight.intensity = 0.75f;
+        }
+        else
+        {
+            tailLight.intensity = 2.37f;
+        }
+    }
+}
