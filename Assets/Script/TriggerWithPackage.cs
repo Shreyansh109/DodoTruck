4e06f83ecb62de1d3ee1aaa06f74dce5900aafe8
Shreyansh <shreyanshgupta118@gmail.com>
2026-04-26 20:56:51 +0530

    add representation for accepted orders

---
+++ Assets/Script/TriggerWithPackage.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TriggerWithPackage : MonoBehaviour
+{
+    bool order;
+    [SerializeField] private float destroyDelay = 0.14f;
+    private void OnTriggerEnter2D(Collider2D other) { 
+        if(other.CompareTag("IcecreamOrder") && !order){
+            Destroy(other.gameObject, destroyDelay);
+            GetComponent<ParticleSystem>().Play();
+            order = true;
+        }
+        if(order && other.CompareTag("Customer")){
+            Debug.Log("Package delivered!");
+            GetComponent<ParticleSystem>().Stop();
+            order = false;
+        }
+    }
+}
