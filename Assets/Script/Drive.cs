422966225963c3dde9fcd4094401893cce9bfd50
Shreyansh <shreyanshgupta118@gmail.com>
2026-04-27 21:21:37 +0530

    add boost

--- Assets/Script/Drive.cs
+++ Assets/Script/Drive.cs
@@ -1,12 +1,14 @@
+using System.Data;
 using System.Data.Common;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Drive : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    float speed;
-    float stearing;
+    float speedVar=20f;
+    float stearing, speed;
     void Start()
     {
         
@@ -16,14 +18,15 @@ public class Drive : MonoBehaviour
     void Update()
     {
         speed=0;
+        
         stearing=0;
         if (Keyboard.current.wKey.isPressed)
         {
-            speed = 20f;
+            speed = speedVar*1f;
         }
         else if (Keyboard.current.sKey.isPressed)
         {
-            speed = -20f;    
+            speed = speedVar*-1f;   
         }
 
        if(Keyboard.current.aKey.isPressed && (Keyboard.current.wKey.isPressed || Keyboard.current.sKey.isPressed))
@@ -37,5 +40,22 @@ public class Drive : MonoBehaviour
         }
         transform.Rotate(0, 0, stearing*Time.deltaTime*speed/20);
         transform.Translate(0, speed*Time.deltaTime, 0);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Boost"))
+        {
+            speedVar=30f;
+            Destroy(other.gameObject, 0.14f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Bump"))
+        {
+            speedVar=20f;
+            Debug.Log("Bump!");
+        }
     }
 }
